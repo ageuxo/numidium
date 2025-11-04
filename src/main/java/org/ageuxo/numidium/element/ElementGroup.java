6d75bb4089bb164868554a1217f79761cda8cc6c
ageuxo <ageuxo@gmail.com>
2025-11-04 22:37:15 +0100

    Add data saving and loading

--- src/main/java/org/ageuxo/numidium/element/ElementGroup.java
+++ src/main/java/org/ageuxo/numidium/element/ElementGroup.java
@@ -1,6 +1,8 @@
 package org.ageuxo.numidium.element;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.ageuxo.numidium.blockentity.ElementBlockEntity;
@@ -22,6 +24,26 @@ public class ElementGroup implements LogicElement {
     public void tick(Level level, BlockPos pos, BlockState state, ElementBlockEntity blockEntity) {
         for (LogicElement element : elements) {
             element.tick(level, pos, state, blockEntity);
+        }
+    }
+
+    @Override
+    public Tag saveData() {
+        CompoundTag tag = new CompoundTag();
+        for (int i = 0; i < elements.size(); i++) {
+            LogicElement element = elements.get(i);
+            tag.put(String.valueOf(i), element.saveData());
+        }
+
+        return tag;
+    }
+
+    @Override
+    public void loadData(Tag tag) {
+        for (int i = 0; i < elements.size(); i++) {
+            LogicElement element = elements.get(i);
+            Tag subTag = ((CompoundTag) tag).get(String.valueOf(i));
+            element.loadData(subTag);
         }
     }
 }
