732e4497c005d6eb8f74c4e8ca25e382ada24500
ageuxo <ageuxo@gmail.com>
2025-11-15 12:54:45 +0100

    Add ElementData serialisation to ElementBlockEntity

--- src/main/java/org/ageuxo/numidium/blockentity/ElementBlockEntity.java
+++ src/main/java/org/ageuxo/numidium/blockentity/ElementBlockEntity.java
@@ -3,18 +3,23 @@ package org.ageuxo.numidium.blockentity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.ageuxo.numidium.element.LogicElement;
+import org.ageuxo.numidium.element.data.ElementDataPatch;
+import org.ageuxo.numidium.element.data.PatchedElementDataMap;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Optional;
 
 @ParametersAreNonnullByDefault
 public class ElementBlockEntity extends BlockEntity {
 
     protected LogicElement element;
+    protected PatchedElementDataMap elementData;
 
     public ElementBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState, LogicElement element) {
         super(type, pos, blockState);
@@ -29,11 +34,14 @@ public class ElementBlockEntity extends BlockEntity {
     protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
         super.saveAdditional(tag, registries);
         tag.put("element", element.saveData());
+        tag.put("elementData", ElementDataPatch.CODEC.encodeStart(NbtOps.INSTANCE, elementData.asPatch()).getPartialOrThrow());
     }
 
     @Override
     protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
         super.loadAdditional(tag, registries);
         element.loadData(tag.get("element"));
+        Optional<ElementDataPatch> patch = ElementDataPatch.CODEC.parse(NbtOps.INSTANCE, tag.get("elementData")).result();
+        elementData = PatchedElementDataMap.fromPatch(elementData, patch.orElse(ElementDataPatch.EMPTY));
     }
 }
