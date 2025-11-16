2718fc45e7a17a676bb72f7c64cec80d3f23e07f
ageuxo <ageuxo@gmail.com>
2025-11-16 12:54:17 +0100

    Add abstract defineElementData() for building elementData

--- src/main/java/org/ageuxo/numidium/blockentity/ElementBlockEntity.java
+++ src/main/java/org/ageuxo/numidium/blockentity/ElementBlockEntity.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.ageuxo.numidium.element.LogicElement;
+import org.ageuxo.numidium.element.data.ElementDataMap;
 import org.ageuxo.numidium.element.data.ElementDataPatch;
 import org.ageuxo.numidium.element.data.PatchedElementDataMap;
 
@@ -16,7 +17,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Optional;
 
 @ParametersAreNonnullByDefault
-public class ElementBlockEntity extends BlockEntity {
+public abstract class ElementBlockEntity extends BlockEntity {
 
     protected LogicElement element;
     protected PatchedElementDataMap elementData;
@@ -25,6 +26,14 @@ public class ElementBlockEntity extends BlockEntity {
         super(type, pos, blockState);
         this.element = element;
     }
+
+    protected ElementDataMap createElementDataPrototype() {
+        ElementDataMap.Builder builder = ElementDataMap.builder();
+        defineElementData(builder);
+        return builder.build();
+    }
+
+    protected abstract void defineElementData(ElementDataMap.Builder builder);
 
     public static void tick(Level level, BlockPos pos, BlockState state, ElementBlockEntity blockEntity) {
         blockEntity.element.tick(level, pos, state, blockEntity);
@@ -42,6 +51,6 @@ public class ElementBlockEntity extends BlockEntity {
         super.loadAdditional(tag, registries);
         element.loadData(tag.get("element"));
         Optional<ElementDataPatch> patch = ElementDataPatch.CODEC.parse(NbtOps.INSTANCE, tag.get("elementData")).result();
-        elementData = PatchedElementDataMap.fromPatch(elementData, patch.orElse(ElementDataPatch.EMPTY));
+        elementData = PatchedElementDataMap.fromPatch(createElementDataPrototype(), patch.orElse(ElementDataPatch.EMPTY));
     }
 }
